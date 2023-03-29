af89c4a2547234cbda08198baac8d6f0738da818
OlebogengBogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2023-03-29 16:02:18 +0200

    IWA3

---
+++ IWA3/challenge2/johannes.js
@@ -0,0 +1,8 @@
+// johannes.js
+
+const firstname = "Johannes"
+const surname = "Potgieter"
+export const role3 = "Intern"
+
+const display= firstname + " " + surname + " (" + role3 + ")"
+document.querySelector('#johannes').innerText = display
---
+++ IWA3/challenge2/scripts.js
@@ -0,0 +1,6 @@
+// scripts.js
+import {role1} from 'nwabisa.js';
+import {role2} from 'alex.js';
+import {role3} from 'johannes.js';
+
+console.log('roles:', role1,role3,role2);
