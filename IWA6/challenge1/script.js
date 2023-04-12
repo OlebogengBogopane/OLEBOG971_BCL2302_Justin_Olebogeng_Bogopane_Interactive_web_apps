c659a720e3332c7e7f84ddf73ca9b7e190458633
OlebogengBogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2023-04-12 09:55:46 +0200

    .

--- IWA6/challenge1/script.js
+++ IWA6/challenge1/script.js
@@ -10,5 +10,7 @@ console.log('Primary phone is valid numerical string:', primaryValid)
 console.log('Secondary phone is valid numerical string:', secondaryValid )
 
 
+
+// !== check whether two operands are not equal then returning a Boolean.
 //  /^\D+$/ test that my variables are not all numbers and parse them as true or false 
 // line 1 has a an O that makes it a string  */
