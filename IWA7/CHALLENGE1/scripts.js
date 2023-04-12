07ff5e357584db89a782450f0ed19b23e2c860b9
OlebogengBogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2023-04-12 10:01:04 +0200

    .

--- IWA7/CHALLENGE1/scripts.js
+++ IWA7/CHALLENGE1/scripts.js
@@ -1,4 +1,4 @@
-const value = "3";
+const value = 3;
 console.log(parseInt(value) + 4 + parseInt(value) );
 
-//parseInt value in brackets which gives us 6
+// changed value from string to number..  added to 4 in console to give us a number 10
