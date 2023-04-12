5670189951ad63975d46076f7cbd2c79cfdc2b32
OlebogengBogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2023-04-12 10:40:34 +0200

    .

--- IWA7/CHALLENGE2/scripts.js
+++ IWA7/CHALLENGE2/scripts.js
@@ -1,4 +1,8 @@
 const nickname= "Timmy";
 const firstname = "Timothy";
 
-console.log(`Good Morning, ${nickname ? nickname : firstname ? firstname:''} !` );
+console.log(`Good Morning, ${nickname || firstname} !` );
+
+//Used string interpolation back ticks
+// || logical or- this returns either value
+//$not a reserved character and calls the jQuery function which selects everything in brackets
