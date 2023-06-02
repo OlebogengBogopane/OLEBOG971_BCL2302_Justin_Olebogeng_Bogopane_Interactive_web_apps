7be7ecad51bdc64d01f182ff2976cf3693bd3a28
OlebogengBogopane <125870308+OlebogengBogopane@users.noreply.github.com>
2023-06-02 12:12:29 +0200

    .

--- IWA1/script.js
+++ IWA1/script.js
@@ -1,20 +1,63 @@
-const MAX_NUMBER = 15
+
+//@ts-check
+
+/**
+  * Maximum allowed number.
+  * @constant {number}
+  */
+
+ const MAX_NUMBER = 15
+
+/**
+ * Minimum allowed number.
+ * @constant {number}
+ */
+
 const MIN_NUMBER = -5
+/**
+ * The number input element.
+ * @type {HTMLInputElement}
+ */
 
 const number = document.querySelector('[data-key="number"]')
+/**
+ * The subtract button element.
+ * @type {HTMLButtonElement}
+ */
+
 const subtract = document.querySelector('[data-key="subtract"]')
+/**
+ * The add button element.
+ * @type {HTMLButtonElement}
+ */
+
+
 const add = document.querySelector('[data-key="add"]')
 
-const subtractHandler = () => {
-    const newValue = parseInt(number.value) - 1
-    number.value = newValue
+
+
+/**
+ * Event handler for the subtract button click event.
+ * @prop {Function} subtractHandler - Event handler for the subtract button click event.
+ * @param {Event} event - The click event object.
+ */
+const subtractHandler = (event) => {
+    /**
+     * The new value after subtracting one from the current number value.
+     * @type {number}
+     * @prop {number} newValue - The new value after subtracting one.
+     */
+    const newValue = parseInt(number.value) - 1;
+
+    number.value = newValue;
 
     if (newValue <= MIN_NUMBER) {
-        subtract.disabled = true
+        subtract.disabled = true;
     } else {
-        subtract.disabled = false
+        subtract.disabled = false;
     }
-}
+};
+
 
 const addHandler = () => {
     const newValue = parseInt(number.value) + 1
